52e8c9daccb52ec90d90ccf9aa60a31cb813fbe0
llt3055 <llt30555@gmail.com>
2026-02-15 14:06:19 +0800

    implement displayarray function and complete display array function

--- lab7.cpp
+++ lab7.cpp
@@ -13,11 +13,21 @@ void displayArray(string*);
 int main(){
     string *names = nullptr;
     names = new string[SIZE];
-    for(int i=0;i<SIZE;i++){
-        *(names + i) = "a";
-    }
+    //for(int i=0;i<SIZE;i++){
+        //*(names + i) = "a";
+    //}
+    *(names) = "ted";
+    *(names + 1) = "jin";
+    *(names + 2) = "joe";
+    *(names + 3) = "tyler";
+    *(names + 4) = "liu";
+    cout << "original name"<< endl;
     displayArray(names);
 
+    string *reverse = reverseArray(names);
+    cout << "reverse array";
+    displayArray(reverse);
+    
     delete[] names;
     names = nullptr;
     return 0;
@@ -34,7 +44,7 @@ string* reverseArray(string *names){
     for (int i= 0;i<SIZE/2;i++){
         string temp = *(names + i);
         *(names + i) = *(names + (SIZE -1 -i));
-        
-
+        *(names + (SIZE -1 -i)) = temp;
     }
+    return names;
 }
